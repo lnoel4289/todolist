825b9917a83f60cc2f2ce23b6c50d2cc718f04ed
Laurent NOËL <lnoel4289@gmail.com>
2023-08-16 17:24:31 +0200

    first commit

---
+++ App.test.jsx
@@ -0,0 +1,9 @@
+// import { describe, expect, it } from "vitest"; not necessary as test.globals: true is enabled in vite.config.js
+import App from "./src/App";
+import { render } from "@testing-library/react";
+
+describe("App", () => {
+  it("should render without crash", () => {
+    render(<App />);
+  });
+});
