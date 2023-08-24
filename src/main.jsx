d8c936da273422fee78b08bb07c7a173338b4a37
Laurent NOËL <lnoel4289@gmail.com>
2023-08-24 15:13:26 +0200

    router ok

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 import "./css/index.css";
 import { Provider } from "react-redux";
 import store from "./store";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import MyTodoList from "./components/MyTodoList";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <React.StrictMode>
-      <App />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MyTodoList />} />
+        </Routes>
+      </BrowserRouter>
     </React.StrictMode>
   </Provider>
 );
