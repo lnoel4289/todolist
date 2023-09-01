5e08f7ef8a59b77d9c7edbe8eca20341393c64a8
Laurent NOËL <lnoel4289@gmail.com>
2023-09-01 17:35:07 +0200

    added App.test.jsx

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,15 +1,24 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import App from "./App";
 
 describe("App", () => {
-
   beforeEach(() => render(<App />));
 
-  it("should render without crash", () => {
-    screen.debug();
-  });
+  it("should keep displayed state values through navigating between pages(state persistance)", () => {
+    const homeLink = screen.getByRole("link", { name: /home/i });
+    const aboutLink = screen.getByRole("link", { name: /about/i });
 
-  it("should keep displayed selectors when navigating between pages", () => {
-    
-  })
+    const textInput = screen.getByLabelText("Enter a task to do here");
+    fireEvent.change(textInput, { target: { value: "i should do this task" } });
+    expect(textInput.value).toBe("i should do this task");
+    fireEvent.click(aboutLink);
+    expect(
+      screen.getByText(
+        /this is an additional page to show that the state persists between pages. You can safely navigate between pages and the current display won't be reset when you come back/i
+      )
+    ).toBeDefined();
+    fireEvent.click(homeLink);
+    expect(textInput.value).toBe("i should do this task");
+    expect(screen.getByDisplayValue(/i should do this task/i)).toBeDefined();
+  });
 });
