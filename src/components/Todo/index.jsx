395a4daa961ff3f985d4de8d96bbee7f64bd4869
Laurent NOËL <lnoel4289@gmail.com>
2023-10-11 20:58:22 +0200

    more css

--- src/components/Todo/index.jsx
+++ src/components/Todo/index.jsx
@@ -6,32 +6,45 @@ const Todo = ({
   testId,
   index,
 }) => {
-  const undoneArticleStyle = "bg-orange-400";
-  const doneArticleStyle = "bg-green-400";
-  const undoneParagStyle = "bg-orange-100";
-  const doneParagStyle = "bg-green-100";
+  const baseArticleStyle = "m-h-14 p-1 rounded border shadow";
+  const undoneArticleStyle = "bg-orange-300 border-orange-500";
+  const doneArticleStyle = "bg-green-300 border-green-500";
+  const baseParagStyle = "grow border p-1 break-words hyphens-auto mt-1"
+  const undoneParagStyle = "bg-orange-100 border-orange-500";
+  const doneParagStyle = "bg-green-100 border-green-500";
+  const baseSpanStyle = "italic px-3 rounded border"
+  const undoneSpanStyle = "text-orange-100 bg-orange-500 border-orange-100";
+  const doneSpanStyle = "text-green-100 bg-green-500 border-green-100";
 
   return (
     <article
       data-testid={testId}
       className={
         isChecked
-          ? `border-2 m-h-14 ${doneArticleStyle}`
-          : `border-2 m-h-14 ${undoneArticleStyle}`
+          ? `${baseArticleStyle} ${doneArticleStyle}`
+          : `${baseArticleStyle} ${undoneArticleStyle}`
       }
     >
-      <span className="border italic">{isChecked ? "Done !" : "Todo"}</span>
+      <span
+        className={
+          isChecked
+            ? `${baseSpanStyle} ${doneSpanStyle} `
+            : `${baseSpanStyle} ${undoneSpanStyle}`
+        }
+      >
+        {isChecked ? "Done !" : "Todo"}
+      </span>
       <div className="flex justify-between">
         <p
           className={
             isChecked
-              ? `grow border-2 break-words hyphens-auto ${undoneParagStyle}`
-              : `grow border-2 break-words hyphens-auto ${doneParagStyle}`
+              ? `${baseParagStyle} ${doneParagStyle}`
+              : `${baseParagStyle} ${undoneParagStyle}`
           }
         >
           {isChecked ? `` : index + 1 + `.`} {string}
         </p>
-        <div className="flex items-center space-x-2">
+        <div className="flex items-center space-x-2 p-1">
           <input type="checkbox" checked={isChecked} onChange={handleChange} />{" "}
           <i
             className="fa-solid fa-trash Class Properties"
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    fontFamily: {
+      'tomatoes': ['Tomatoes']
+    },
     extend: {},
   },
   plugins: ["prettier-plugin-tailwindcss"],
