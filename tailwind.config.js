ea1c68af726c28de021582b48b01f899f392be55
Laurent NOËL <lnoel4289@gmail.com>
2023-10-11 22:12:16 +0200

    more css

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     fontFamily: {
-      'tomatoes': ['Tomatoes']
+      'indie': ['Indie Flower']
     },
     extend: {},
   },
