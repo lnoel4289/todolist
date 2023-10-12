87be4d58b0dbf561bda8868eaf90f115a700ed8d
Laurent NOËL <lnoel4289@gmail.com>
2023-10-12 15:47:39 +0200

    problem solved

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,9 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     fontFamily: {
-<<<<<<< HEAD
       indie: ["Indie Flower"],
       roboto: ["Roboto"],
-=======
       'indie': ['Indie Flower']
->>>>>>> ea1c68af726c28de021582b48b01f899f392be55
     },
     extend: {},
   },
