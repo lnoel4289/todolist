884fee581b72fd2728a711f8b6053c0e55f5b1d1
Laurent NOËL <lnoel4289@gmail.com>
2024-02-01 18:28:21 +0100

    toggle switches front ok

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -6,7 +6,7 @@ const Header = () => {
       <h1 className="sm:relative top-4 sm:h-16 grow-[2] xs:grow-[5] sm:grow-[8] font-pacifico text-lg xs:text-2xl sm:text-4xl">
         my_TODOList
       </h1>
-      <div className="grow flex flex-col justify-around h-full">
+      <div className="grow flex flex-col justify-between h-full p-0.5">
         <nav className="font-bold sm:text-2xl sm:relative top-3">
           <ul className="flex justify-between items-center text-white uppercase text-lg sm:text-2xl">
             <li>
@@ -32,22 +32,28 @@ const Header = () => {
           </ul>
         </nav>
         <div className="grid grid-cols-2">
-          <label htmlFor="tgl-drk" className="relative h-4 w-8">
-            <input
-              type="checkbox"
-              id="tgl-drk"
-              className="w-0 h-0 opacity-0 peer"
-            ></input>
-            <span className="absolute inset-0 bg-gray-300 cursor-pointer before:flex before:absolute before:w-3.5 before:h-3.5 before:bg-white before:top-px before:left-px before:duration-300 before:peer-checked:translate-x-4"></span>
-          </label>
-          <label htmlFor="tgl-lng" className="relative h-4 w-8">
-            <input
-              type="checkbox"
-              id="tgl-lng"
-              className="w-0 h-0 opacity-0 peer"
-            ></input>
-            <span className="absolute inset-0 bg-gray-300 cursor-pointer before:flex before:absolute before:w-3.5 before:h-3.5 before:bg-white before:top-px before:left-px before:duration-300 before:peer-checked:translate-x-4"></span>
-          </label>
+          <div className="flex items-center justify-center">
+            <label htmlFor="tgl-lng" className="relative h-4 w-8">
+              <input
+                type="checkbox"
+                id="tgl-lng"
+                className="w-0 h-0 opacity-0 peer"
+              ></input>
+              <span className="absolute inset-0 bg-gray-300 rounded-full cursor-pointer before:flex before:absolute before:w-3 before:h-3 before:rounded-full before:bg-white before:top-0.5 before:left-0.5 before:duration-300 before:peer-checked:translate-x-4"></span>
+            </label>
+            <span className="text-white">FR</span>
+          </div>
+          <div className="flex items-center justify-center">
+            <label htmlFor="tgl-drk" className="relative h-4 w-8">
+              <input
+                type="checkbox"
+                id="tgl-drk"
+                className="w-0 h-0 opacity-0 peer"
+              ></input>
+              <span className="absolute inset-0 bg-gray-300 rounded-full cursor-pointer before:flex before:absolute before:w-3 before:h-3 before:rounded-full before:bg-white before:top-0.5 before:left-0.5 before:duration-300 before:peer-checked:translate-x-4"></span>
+            </label>
+            <span>🌙</span>
+          </div>
         </div>
       </div>
     </header>
