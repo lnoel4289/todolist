58743b9aaa38b54aec4ec4c2b2d2b88c4b4eebb8
Laurent NOËL <lnoel4289@gmail.com>
2024-02-05 12:26:34 +0100

    finished toggle switch props tests

--- src/components/ToggleSwitch/index.test.jsx
+++ src/components/ToggleSwitch/index.test.jsx
@@ -1,15 +1,18 @@
 import { render, screen } from "@testing-library/react";
 import ToggleSwitch from ".";
-import { beforeEach } from "vitest";
+// import { beforeEach } from "vitest";
 
 describe("ToggleSwitch", () => {
   beforeEach(() => {
     render(<ToggleSwitch id="1234" text="toggle-text" />);
   });
+
   it("should render props", () => {
     const text = screen.getByText(/oggle-text/);
     expect(text).toBeDefined();
-    const id = screen.getByAltText();
-    expect(id).toBe("1234")
+    const node = document.getElementById("1234");
+    expect(node.id).toBe("1234");
   });
+
+  it("should modify state on change", () => {})
 });
